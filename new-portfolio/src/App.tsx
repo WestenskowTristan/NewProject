015ee96f641998c2a3e6a731e6beac19d30c39cd
tristan westenskow <tristanwestenskow@tristans-MacBook-Pro.local>
2025-09-04 17:22:35 -0600

    project started

---
+++ new-portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import PageContainer from "./components/common/PageContainer"
+
+function App() {
+
+  return (
+    <PageContainer>
+      hello world
+    </PageContainer>
+  )
+}
+
+export default App
